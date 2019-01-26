ebda754879225b443622c0ace6f00622526bb991
Kyle Lucke <luckek1354@gmail.com>
2019-01-26 00:23:28 -0700

    Removed mapped and unecessary comments

--- src/python/MainWindow.py
+++ src/python/MainWindow.py
@@ -12,29 +12,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Set up the user interface from Designer.
         self.setupUi(self)
 
-        # # Connect up the buttons.
-        # self.okButton.clicked.connect(self.accept)
-        # self.cancelButton.clicked.connect(self.reject)
-
-        # Create mapper
-        self.mapper = QtCore.QSignalMapper(self)
-
-        # NOTE: this has to be done after retranslateUi otherwise the text is not set
         for child in self.menubar.children():
             if type(child) is QtWidgets.QMenu:
                 for action in child.actions():
-                    # This line maps an action to a string
-                    self.mapper.setMapping(action, action.text())
                     identifier = action.text()
                     action.triggered.connect(lambda state, x=identifier: self.handle_button(x))
-
-                    # Connect the action's signal to the mapper
-                    # action.triggered.connect(self.mapper.map)
-
-        # Connect the mapper to the button
-        # self.mapper.connect(self.handle_button)
-        # self.mapper.mapped['QString'].connect(self.handle_button)
-        # self.mapper.mapped['QString'].connect(self.handle_file_button)
 
     # FIXME: make static or remove from class altogether if we do not need to access anything in main window
     @QtCore.pyqtSlot(str)
