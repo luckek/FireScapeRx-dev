0ecf2ffa627c5e46782a0325b66e011d87643646
Kyle Lucke <luckek1354@gmail.com>
2019-01-26 00:19:06 -0700

    Simplified app.py

--- src/python/app.py
+++ src/python/app.py
@@ -1,16 +1,6 @@
 import sys
+from PyQt5.QtWidgets import QApplication
 from MainWindow import MainWindow
-from gui.UserSettingsForm import *
-from PyQt5.QtWidgets import QMainWindow, QApplication
-
-
-# class AppWindow(QMainWindow):
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.ui = MainWindow()
-#         # self.ui.setupUi(self)
-#         self.show()
 
 
 def main(argv):
