b3e289613562bce04051bca4efe4f3fca6149935
Kyle Lucke <luckek1354@gmail.com>
2019-02-04 14:16:58 -0700

    Added some utility functions to get filenames, etc.

--- src/python/Utility.py
+++ src/python/Utility.py
@@ -1,3 +1,4 @@
+import os
 
 
 # FIXME: can rename to more useful method name
@@ -16,3 +17,39 @@ def validate(text):
         return 0  # too long -> "invalid"
 
     return 2  # Valid text
+
+
+def get_filename(path, ext=False):
+    """Returns filename in given path, strips the extension off is ext is False"""
+
+    fname = path.split(os.sep)[-1]
+
+    if ext:
+        return fname
+
+    return fname.split('.')[0]
+
+
+def get_filename_ext(path):
+    """Returns last present extension on filename, if one exists"""
+
+    fname_ext_list = get_filename(path, True).split('.')
+
+    # No extension
+    if len(fname_ext_list) < 2:
+        return ''
+
+    return fname_ext_list[-1]
+
+
+def make_unique_directory(directory):
+    """Ensures the given directory is unique by appending a unique identifier to it"""
+
+    if os.path.exists(directory):
+        uid = 0
+        while os.path.exists(directory + str(uid)):
+            uid += 1
+
+        directory += str(uid)
+
+    return directory
