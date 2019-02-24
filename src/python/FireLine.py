9b29c9423e95c1aba97b14ce51f8417c433c4a72
Kyle Lucke <luckek1354@gmail.com>
2019-02-24 16:34:07 -0700

    Created simple class to hold info about fire lines

---
+++ src/python/FireLine.py
@@ -0,0 +1,43 @@
+from Point import Point
+
+
+class FireLine:
+
+    def __init__(self, points_list, times):
+
+        assert(len(points_list) == len(times))
+
+        self._points_list = points_list
+
+        self._times_list = times
+
+    def points_list(self):
+        return self._points_list
+
+    def points_set(self):
+        return set(self._points_list)
+
+    def times_list(self):
+        return self._times_list
+
+    def times_set(self):
+        return set(self._times_list)
+
+    def overlap(self, rhs):
+        """
+        Simple function to test if two firelines overlap
+        :param rhs: the fireline we wish to check for intersection with
+        :return: true if there is no overlap between the respective firelines(i.e. a non-empty intersection)
+        """
+
+        return bool(self.points_set().intersection(rhs.points_set()))
+
+    def same(self, rhs):
+        return self.points_set() == rhs.points_set()
+
+    def __getitem__(self, i):
+
+        return self._points_list[i], self._times_list[i]
+
+    def __str__(self):
+        return str(self._points_list) + '\n' + str(self._times_list)
