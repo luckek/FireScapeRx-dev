b536b75d1b17d2a14fc0cdfbdaf8ed95e6c87409
Kyle Lucke <luckek1354@gmail.com>
2019-03-02 12:59:06 -0700

    User can no longer edit 'no value' data cells

--- src/python/FuelMapEditorButton.py
+++ src/python/FuelMapEditorButton.py
@@ -13,3 +13,7 @@ class FuelMapEditorButton(Ui_AsciiGridEditorButton):
     def __init__(self, parent, bttn_size, init_color=1, name=None):
 
         super().__init__(parent, bttn_size, init_color, name)
+
+        # Disallow user to edit 'no value' cells
+        if init_color == -1:
+            self.clicked.disconnect(self.button_click)
